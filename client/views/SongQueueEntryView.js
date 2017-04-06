57206bcead3f15d061b6706e897d6cf86fc1c5e8
ArseniyKotov <kotov.a.arseniy@gmail.com>
2017-04-06 11:10:46 -0700

    song queue now reneders

--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -1,6 +1,8 @@
 // SongQueueEntryView.js - Defines a backbone view class for the song queue entries.
 var SongQueueEntryView = Backbone.View.extend({
   
+  tagName: 'tr',
+  
   template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
 
   initialize: function() {
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,21 +1,23 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
   
+  tagName: 'table',
+  
   collection: SongQueue,
 
   initialize: function() {
+    this.$el.empty();
     this.render();
+    this.collection.on('ended add dequeue', this.render, this);
   },
 
   render: function() {
-    // this.$el.empty();
-    this.collection.forEach(this.renderSongQueue, this);
-    // return this.$el;
-  },
-
-  renderSongQueue: function(song) {
-    var songQueueEnteryView = new SongQueueEntryView({model: song});
-    this.$el.append(songQueueEnteryView.render());
+    this.$el.children().detach();
+    this.$el.html('<th>Song Queeue</th>').append(this.collection.map(function(song) {
+      return new SongQueueEntryView({ model: song }).render();
+    }));
+    
+    return this.$el;
   }
 
 });
